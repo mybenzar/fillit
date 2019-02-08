aaf1959308e63f03d9c1c7f49bc47d1cef6acc01
Myriam BENZARTI <mybenzar@e2r9p5.42.fr>
2019-02-08 13:50:11 +0100

    remise de ft browse a sa derniere version

--- ft_browse.c
+++ ft_browse.c
@@ -6,20 +6,23 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 14:10:45 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/02/08 13:36:09 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/02/08 13:49:52 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <stdio.h>
 
-char	**ft_browse(char **tab, t_triminos *list, int size_tab)
+char **ft_browse(char **tab, t_triminos *list, int size_tab)
 {
-	t_triminos	*tmp;
-	int			tri_placed;
-	int 		i;
-	char		letter;
-	int			stepback;
+	t_triminos *tmp;
+	int   tri_placed;
+	int   i;
+	char  letter;
+	int   stepback;
+	int   flag;
 
+	flag = 0;
 	tri_placed = 0;
 	tmp = list;
 	tab = ft_create_tab(size_tab);
@@ -27,23 +30,35 @@ char	**ft_browse(char **tab, t_triminos *list, int size_tab)
 	stepback = 0;
 	while (tri_placed < ft_list_size(list))
 	{
-		if (!stepback && ft_place_bis(tmp, tab, 0, 0))
+		if (!stepback && ft_place(tmp, tab, 0, 0))
 		{
+			ft_putendl("rentre dans la boucle ft_place = 1");
 			tri_placed++;
 			letter = tmp->letter;
 			tmp = tmp->next;
+			ft_display_tab(tab);
 		}
 		else
 		{
+			printf("TEST on rentre dans le else, lettre = %c\n", letter);
 			if (ft_del(letter, tab))
-				tri_placed--;
+			{
+				flag = 1;
+				printf("rentre dans if ft_del\n");
+				printf("del de %c\n", letter);
+				ft_display_tab(tab);
+			}
 			stepback = 0;
 			ft_lst_sort(list, tri_placed + 1);
+			ft_display_tri_lst(list);
 			if (ft_next_valid_letter(list, letter, tab))
 			{
+				printf("list a valid letter, et tmp->letter = %c\n", tmp->letter);
 				letter = ft_next_valid_letter(list, letter, tab);
+				printf("next valid letter : %c, tri_placed : %i\n", letter, tri_placed);
 				list = ft_lst_insert(list, tri_placed, letter);
 				ft_lst_sort(list, tri_placed + 1);
+				ft_display_tri_lst(list);
 				tmp = list;
 				while (tmp->letter != letter && tmp->next)
 					tmp = tmp->next;
@@ -53,12 +68,14 @@ char	**ft_browse(char **tab, t_triminos *list, int size_tab)
 				if (tri_placed == 0)
 					break ; 
 				letter = ft_find_letter(list, tri_placed);
+				printf("stepback sur letter %c\n", letter);
 				stepback = 1;
 			}
 		}
 	}
 	if (ft_list_size(list) == tri_placed)
 		return (tab);
+	ft_putendl("tableau trop petit \n");
 	ft_free_tab(tab);
 	size_tab++;
 	list = ft_lst_insert(list, 0, 'A');
